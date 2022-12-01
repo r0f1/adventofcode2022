6970cb141dc0f5555fe62696d745ae459f69b921
Florian Rohrer <semicolonator@gmail.com>
2022-12-01 06:22:10 +0100

    Day 01

--- day01/main.py
+++ day01/main.py
@@ -1,1 +1,6 @@
-print("hello world!")
+with open("input.txt") as f:
+    cals = f.read().rstrip()
+
+elves = [sum(int(c) for c in e.split("\n")) for e in cals.split("\n\n")]
+print(max(elves))
+print(sum(sorted(elves, reverse=True)[:3]))
