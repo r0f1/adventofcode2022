14ff4cfb9ebc3783e419ff4c29841540ede71ddf
Florian Rohrer <semicolonator@gmail.com>
2022-12-01 06:28:56 +0100

    Day 1

--- day01/main.py
+++ day01/main.py
@@ -1,6 +1,5 @@
 with open("input.txt") as f:
     cals = f.read().rstrip()
-
 elves = [sum(int(c) for c in e.split("\n")) for e in cals.split("\n\n")]
 print(max(elves))
-print(sum(sorted(elves, reverse=True)[:3]))
+print(sum(sorted(elves)[-3:]))
