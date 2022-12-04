186119b55b0330519a4f76892dec93cc92701434
Christian Finnberg <christian@finnberg.net>
2022-12-05 00:02:26 +0200

    Update main.py
    I came to a quite similar solution than you, but the my second part was simpler

--- day04/main.py
+++ day04/main.py
@@ -2,7 +2,4 @@ with open("input.txt") as f:
     lines = [[[int(i) for i in k.split("-")] for k in x.split(",")] for x in f]
 
 print(sum(a <= c <= d <= b or c <= a <= b <= d for (a,b), (c,d) in lines))
-print(sum(any([a <= c <= b, a <= d <= b, c <= a <= d, c <= b <= d]) for (a,b), (c,d) in lines))
-
-# Alternative for Part 2
-print(sum([max(a,c) <= min(b,d) for (a,b), (c,d) in lines]))
+print(sum(a <= d and b >= c for (a,b), (c,d) in lines))
