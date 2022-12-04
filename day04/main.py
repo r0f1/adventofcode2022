3e9977b8cdec17d4588aca5a2ffb89c5014ca781
Florian Rohrer <semicolonator@gmail.com>
2022-12-04 22:50:46 +0100

    Day 4

--- day04/main.py
+++ day04/main.py
@@ -1,5 +1,8 @@
 with open("input.txt") as f:
     lines = [[[int(i) for i in k.split("-")] for k in x.split(",")] for x in f]
 
-print(sum((a <= c <= b and a <= d <= b) or (c <= a <= d and c <= b <= d) for (a,b), (c,d) in lines))
-print(sum(any([a <= c <= b, a <= d <= b, c <= a <= d, c <= b <= d])      for (a,b), (c,d) in lines))
+print(sum(a <= c <= d <= b or c <= a <= b <= d for (a,b), (c,d) in lines))
+print(sum(any([a <= c <= b, a <= d <= b, c <= a <= d, c <= b <= d]) for (a,b), (c,d) in lines))
+
+# Alternative for Part 2
+print(sum([max(a,c) <= min(b,d) for (a,b), (c,d) in lines]))
