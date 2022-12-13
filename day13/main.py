819a3199fbbd2341369c38f9f725e64247927a9d
Florian Rohrer <semicolonator@gmail.com>
2022-12-13 20:19:40 +0100

    Day 13

--- day13/main.py
+++ day13/main.py
@@ -5,26 +5,21 @@ with open("input.txt") as f:
     pairs = [[eval(l) for l in x.split()] for x in f.read().split("\n\n")]
 
 def is_correct_order(l, r):
-    if type(l) == type(r):
-        if type(l) == int and type(r) == int:
-            return "Unsure" if l == r else str(l < r)
-        else:
-            if len(l) == 0 and len(r) == 0: return "Unsure"
-            if len(l) == 0:                 return "True"
-            if len(r) == 0:                 return "False"
-            r1 = is_correct_order(l[0], r[0])
-            return r1 if r1 != "Unsure" else is_correct_order(l[1:], r[1:])
-
+    if type(l) == int and type(r) == int:
+        return l - r
+    if type(l) == list and type(r) == list:
+        if len(l) == 0 and len(r) == 0: return 0
+        if len(l) == 0:                 return -1
+        if len(r) == 0:                 return 1
+        r1 = is_correct_order(l[0], r[0])
+        return r1 if r1 != 0 else is_correct_order(l[1:], r[1:])
     return is_correct_order([l], r) if type(l) == int else is_correct_order(l, [r])
 
-print(sum(i for i, (l, r) in enumerate(pairs, start=1) if is_correct_order(l, r) == "True"))
+print(sum(i for i, (l, r) in enumerate(pairs, start=1) if is_correct_order(l, r) < 0))
 
 pairs = list(flatten(pairs))
 pairs.extend([[[2]],[[6]]])
 
-def compare(a, b):
-    return {"True": -1, "Unsure": 0, "False": 1}[is_correct_order(a, b)]
-
-pairs_sorted = sorted(pairs, key=cmp_to_key(compare))
+pairs_sorted = sorted(pairs, key=cmp_to_key(is_correct_order))
 i1, i2 = pairs_sorted.index([[2]])+1, pairs_sorted.index([[6]])+1
 print(i1*i2)
